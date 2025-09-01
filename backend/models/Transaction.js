8021027b193f03b75011dccbf27df49cb7c32e15
pateb9298 <patelbhakti9298@gmail.com>
2025-09-01 09:51:01 -0400

    edit db

--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -1,13 +1,21 @@
 const mongoose = require("mongoose");
 
 const TransactionSchema = new mongoose.Schema({
-    merchant:  {type: String, required: true},
-    amount: {type: Number, required: true},
-    category: {type: String, required: true},
-    paymentMethod: {type: String, required: true},
-    date: {type: Date, required: true},
-    location: {type: String, required: true},
-    description: {type: String}
+    Age: {type: Number, required: true},
+    Amount: {type: Number, required: true},
+    Currency: {type: String, required: true},
+    "Shipping Address": {type: String, required: true},
+    Gender: {type: String, required: true},
+    "Type of Card": {type: String, required: true},
+    "Entry Mode": {type: String, required: true},
+    "Type of Transaction": {type: String, required: true},
+    "Merchant Group": {type: String, required: true},
+    "Country of Transaction": {type: String, required: True},
+    "Country of Residence": {type: String, required: True},
+    Bank: {type: String, required: true},
+    Date: {type: Date, required: true},
+    Time: {type: String, required: true},
+    Description: {type: String}
 }, { timestamps: true });
 
 module.exports = mongoose.model("Transaction", TransactionSchema);
